b376fae8ebabdb67d05e7cdbe6bfeebf4422ef2b
friendly-queue-6446 <leslie.sage@gmail.com>
2019-03-16 19:03:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// add solution here
+function theBeatlesPlay (musicians,insruments) {
+  var counter = 0
+  var arr = []
+  for (;musicians[counter];) {
+    arr.push(musicians[counter] + ' plays ' + insruments[counter])
+    counter++
+  }
+  return arr
+}
+
+function johnLennonFacts (facts) {
+  var arr = []
+  var i = 0
+  while (i < facts.length) {
+    arr.push(facts[i] + '!!!')
+    i++
+  }
+  return arr
+}
+
+function iLoveTheBeatles (num) {
+  var myLength = Math.abs(15-num)
+  var arr = []
+  do {
+    arr.push('I love the Beatles!')
+    myLength--
+  }
+  while (myLength > 0)
+  return arr
+}
